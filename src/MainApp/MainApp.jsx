dc7155fa39d19ae0ed9752e17358d5349997e85f
Liran BG <liranbg@gmail.com>
2018-01-30 11:13:34 +0200

    boilerpoint - 1

---
+++ src/MainApp/MainApp.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './MainApp.css';
+
+class MainApp extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1 className="App-Title">React-Parcel Example</h1>
+            </div>
+        );
+    }
+}
+
+export default MainApp;
