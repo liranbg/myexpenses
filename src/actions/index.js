33ce15ec359d8243f81fea587b779ecc6c33d1df
Liran BG <liranbg@gmail.com>
2018-02-02 10:27:06 +0200

    moved store to provider

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,25 +5,36 @@ export const REMOVE_TAG = 'REMOVE_TAG';
 export const SET_TAG = 'SET_TAG';
 export const UNSET_TAG = 'UNSET_TAG';
 
+export const FILTER_EXPENSES_BY_TAG = 'FILTER_EXPENSES_BY_TAG';
+export const REM_FILTER_EXPENSES_BY_TAG = 'REM_FILTER_EXPENSES_BY_TAG';
+
 export function addTag(tag) {
-  return { type: ADD_TAG, payload: tag };
+  return {type: ADD_TAG, payload: tag};
 }
 
 export function deleteTag(tagKey) {
-  return { type: REMOVE_TAG, payload: { key: tagKey } };
+  return {type: REMOVE_TAG, payload: {key: tagKey}};
+}
+
+export function filterExpensesByTag(tagName) {
+  return {type: FILTER_EXPENSES_BY_TAG, payload: {'tagName': tagName}};
+}
+
+export function remFilterExpensesByTag(tagName) {
+  return {type: REM_FILTER_EXPENSES_BY_TAG, payload: {'tagName': tagName}};
 }
 
 export function incTagUses(tagKey) {
-  return { type: INC_TAG, payload: { key: tagKey } };
+  return {type: INC_TAG, payload: {key: tagKey}};
 }
 
 export function setExpenseTag(expenseKey, tagName, applyForAll = false) {
   return {
     type: SET_TAG,
-    payload: { key: expenseKey, tag: tagName, applyForAll: applyForAll }
+    payload: {key: expenseKey, tag: tagName, applyForAll: applyForAll}
   };
 }
 
 export function unsetExpenseTag(expenseKey) {
-  return { type: UNSET_TAG, payload: { key: expenseKey } };
+  return {type: UNSET_TAG, payload: {key: expenseKey}};
 }
--- src/index.js
+++ src/index.js
@@ -2,27 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import MainApp from './pages/MainApp/MainApp';
-import { BrowserRouter } from 'react-router-dom';
 import { createStore } from 'redux';
 import expensesApp from './reducers/index';
+import { Provider } from 'react-redux';
 
 const expenses = require('./mocks/expenses');
 const tags = require('./mocks/tags');
 
-const store = createStore(expensesApp, { expenses: expenses, tags: tags });
+const store = createStore(expensesApp, {
+  expenses: expenses,
+  tags: tags,
+  expensesView: {
+    filterTags: []
+  }
+});
 
 ReactDOM.render(
-  <BrowserRouter>
-    <MainApp store={store} />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <MainApp/>
+  </Provider>,
   document.getElementById('root')
 );
 
 if (module.hot) {
-  module.hot.dispose(function () {
-    // module is about to be replaced
-  });
-  module.hot.accept(function () {
-    // module or one of its dependencies was just updated
-  });
+  module.hot.dispose(() => ({}));
+  module.hot.accept(() => ({}));
 }
