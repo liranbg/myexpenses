2ec4099e2a0ef5e53aa9b3f17f03120cbf903f90
Liran BG <liranbg@gmail.com>
2018-02-06 13:35:27 +0200

    removed unused functions

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,15 +1,12 @@
+import _ from 'lodash';
+
 export function expensesToTagsUses(expenses) {
   let counts = {};
-  const sorted = expenses.map(expense => expense.tag).sort();
-  sorted.forEach(x => (counts[x] = (counts[x] || 0) + 1));
+  expenses
+    .map(expense => expense.tag)
+    .sort()
+    .forEach(x => (counts[x] = (counts[x] || 0) + 1));
   return counts;
-}
-
-export function firebaseTagsToArray(tags) {
-  if (!tags) return [];
-  let arrTagsValues = Object.values(tags);
-  Object.keys(tags).forEach((key, index) => (arrTagsValues[index].key = key));
-  return arrTagsValues;
 }
 
 export function getExpensesFilterByTags(expenses, tags) {
