89fb7398a19de0df1b31d24ca67918acc3d2fd9a
Liran BG <liranbg@gmail.com>
2018-02-06 11:16:11 +0200

    removed unused tags reducer actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,8 @@
-export const SET_TAGS = 'SET_TAGS';
-export const REMOVE_TAG = 'REMOVE_TAG';
-
 export const SET_TAG = 'SET_TAG';
 export const UNSET_TAG = 'UNSET_TAG';
 
 export const FILTER_EXPENSES_BY_TAG = 'FILTER_EXPENSES_BY_TAG';
 export const REM_FILTER_EXPENSES_BY_TAG = 'REM_FILTER_EXPENSES_BY_TAG';
-
-export function deleteTag(tagKey) {
-  return { type: REMOVE_TAG, payload: { key: tagKey } };
-}
 
 export function filterExpensesByTag(tagName, replaceAll = false) {
   return {
@@ -20,13 +13,6 @@ export function filterExpensesByTag(tagName, replaceAll = false) {
 
 export function remFilterExpensesByTag(tagName) {
   return { type: REM_FILTER_EXPENSES_BY_TAG, payload: { tagName: tagName } };
-}
-
-export function setTags(tags) {
-  return {
-    type: SET_TAGS,
-    payload: tags
-  };
 }
 
 export function setExpenseTag(expenseKey, tagName, applyForAll = false) {
--- src/reducers/tags/tags.js
+++ src/reducers/tags/tags.js
@@ -1,11 +1,5 @@
-import { REMOVE_TAG, SET_TAGS } from '../../actions';
-
 const tagsReducer = (state = [], action) => {
   switch (action.type) {
-    case SET_TAGS:
-      return [...action.payload];
-    case REMOVE_TAG:
-      return [...state.filter(f => f.key !== action.payload.key)];
     default:
       return state;
   }
