47dba0329658f75a159a859b1df896daf5019fec
Liran BG <liranbg@gmail.com>
2018-02-24 15:33:25 +0200

    feat(AddExpenses): Expenses Import Page

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import moment from 'moment';
+import * as md5 from 'md5';
 
 export function expensesToTagsUses(expenses) {
   return Object.assign(
@@ -29,7 +30,7 @@ export function dateRangeToLabels(startDate, endDate, stepType = 'month') {
   let endIterator = moment(endDate).endOf(stepType);
   while (startIterator.isSameOrBefore(endIterator)) {
     labels.push(moment(startIterator));
-      startIterator = startIterator.add(1, stepType);
+    startIterator = startIterator.add(1, stepType);
   }
   return labels;
 }
@@ -38,4 +39,52 @@ export function getFilteredExpensesByDates(expenses, fromDate, toDate) {
   return expenses.filter(expense =>
     expense.date.isBetween(fromDate, toDate, null, '[]')
   );
+}
+
+function currencySignToText(sign) {
+  switch (sign) {
+    case '₪':
+      return 'ILS';
+    case '$':
+      return 'USD';
+    case '€':
+      return 'EUR';
+    default:
+      return 'UNKNOWN';
+  }
+}
+
+export function buildExpensesByRows(rows) {
+  // ILS Transaction
+  //["07/12/2017", "GETT", "46", "₪", "46", "₪", "3425519", ""]
+  // Foreign Transaction
+  //["02/01/2018", "04/01/2018", "ONLINE", "39.99", "€", "39.99", "€"]
+  const regexDate = /\d{2}\/\d{2}\/\d{4}/i;
+  const dateIndex = 0,
+    nameIndex = 1,
+    amountIndex = 4,
+    currencyIndex = 5,
+    idIndex = 6,
+    notesIndex = 7;
+  return rows
+    .filter(r => !!r[dateIndex].match(regexDate) && r[nameIndex] !== '')
+    .map(r => {
+      if (r[dateIndex].match(regexDate) && r[dateIndex + 1].match(regexDate))
+        // Foreign Transaction
+        return [...r.slice(1), md5.default(r.slice(1, 5).join()), ''];
+      return [
+        ...r.slice(0, 6),
+        md5.default(r.slice(0, 4).join() + r[idIndex]),
+        r[notesIndex]
+      ];
+    })
+    .map(r => ({
+      id: r[idIndex],
+      name: r[nameIndex],
+      date: moment(r[dateIndex], 'DD/MM/YYYY'),
+      tag: 'Untagged',
+      amount: parseFloat(r[amountIndex]),
+      notes: r[notesIndex],
+      currency: currencySignToText(r[currencyIndex])
+    }));
 }
--- src/pages/MainApp/index.jsx
+++ src/pages/MainApp/index.jsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router';
 import ExpensesPage from '../Expenses';
 import ChartsPage from '../Charts';
 import TagsPage from '../Tags';
+import AddExpensesPage from '../AddExpenses';
 import Header from '../../components/Header';
 import SignIn from '../../components/SignIn';
 import { firestoreConnect } from 'react-redux-firebase';
@@ -35,24 +36,34 @@ class MainApp extends Component {
   render() {
     const { profile } = this.props;
     return (
-      <div className="App">
-        <Header />
-        {profile.isEmpty ? (
-          <div>
-            <SignIn />
-          </div>
-        ) : (
-          <React.Fragment>
-            <Route exact path="/expenses" component={ExpensesPage} />
-            <Route exact path="/tags" component={TagsPage} />
-            <Route exact path="/charts" component={ChartsPage} />
-          </React.Fragment>
-        )}
-        <Segment
-          vertical
-          style={{ margin: '1em 0em 0em', padding: '1em 0em' }}
+      <React.Fragment>
+        <div className="App">
+          <Header />
+          {profile.isEmpty ? (
+            <div>
+              <SignIn />
+            </div>
+          ) : (
+            <React.Fragment>
+              <Route exact path="/expenses" component={ExpensesPage} />
+              <Route exact path="/tags" component={TagsPage} />
+              <Route exact path="/charts" component={ChartsPage} />
+              <Route exact path="/addexpenses" component={AddExpensesPage} />
+            </React.Fragment>
+          )}
+        </div>
+        <div
+          style={{
+            position: 'absolute',
+            right: 0,
+            bottom: 0,
+            left: 0,
+            padding: '1em 1em',
+            textAlign: 'center',
+            margin: '1em 1em 1em'
+          }}
         />
-      </div>
+      </React.Fragment>
     );
   }
 }
