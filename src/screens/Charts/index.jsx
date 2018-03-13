f30b40436de9914fa3b01f85b177a5d55d59d946
Liran BG <liranbg@gmail.com>
2018-03-13 16:18:02 +0200

    WIP

--- src/screens/Charts/index.jsx
+++ src/screens/Charts/index.jsx
@@ -4,14 +4,19 @@ import PropTypes from 'prop-types';
 import { Segment, Header, CardGroup, Container, Button, SegmentGroup } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { Expense, Tag } from '../../proptypes';
-import { expensesDatesLuxonify, getFilteredExpensesByDates, slugify, tagsToHierarchy } from '../../helpers';
+import {
+	expensesDatesLuxonify,
+	getFilteredExpensesByDates,
+	slugify,
+	tagsToHierarchy
+} from '../../helpers';
 import { compose } from 'redux';
 import ChartDateSelection from '../../components/Charts/ChartDateSelection';
 import BarChartCard from '../../components/Charts/BarChartCard';
 import PieChartCard from '../../components/Charts/DoughnutChartCard';
 import { setDatesRange } from '../../actions';
 import { firestoreConnect } from 'react-redux-firebase';
-import TagsSelection from "../../components/TagsSelection";
+import TagsSelection from '../../components/TagsSelection';
 
 export class ChartsPage extends Component {
 	static propTypes = {
@@ -62,20 +67,25 @@ export class ChartsPage extends Component {
 		return aggregatedTags;
 	};
 
-    handleSelectTag = (e, {value}) => {
-    	this.setState({selectedTag:value})
+	handleSelectTag = (e, { value }) => {
+		this.setState({ selectedTag: value });
 	};
 
-    renderSingleTagCharts(groupedExpenses, tagsHierarchy) {
-        const houseTag = tagsHierarchy.find(t => t.id === slugify(this.state.selectedTag));
-        const houseTags = [houseTag, ...houseTag.children];
-        return (<React.Fragment>
-            <BarChartCard
-                expenses={this.aggregateExpensesTagsByParent(groupedExpenses, houseTag)}
-                tags={houseTags}
-            />
-            <PieChartCard expenses={this.aggregateExpensesTagsByParent(groupedExpenses, houseTag)} tags={houseTags} />
-        </React.Fragment>)
+	renderSingleTagCharts(groupedExpenses, tagsHierarchy) {
+		const houseTag = tagsHierarchy.find(t => t.id === slugify(this.state.selectedTag));
+		const houseTags = [houseTag, ...houseTag.children];
+		return (
+			<React.Fragment>
+				<BarChartCard
+					expenses={this.aggregateExpensesTagsByParent(groupedExpenses, houseTag)}
+					tags={houseTags}
+				/>
+				<PieChartCard
+					expenses={this.aggregateExpensesTagsByParent(groupedExpenses, houseTag)}
+					tags={houseTags}
+				/>
+			</React.Fragment>
+		);
 	}
 
 	render() {
@@ -97,26 +107,35 @@ export class ChartsPage extends Component {
 						<Button basic primary onClick={this.narrowDatesByExpenses} content={'Narrow Dates'} />
 					</Segment>
 					<Segment.Group horizontal>
-
-                        <Segment >
-							<TagsSelection tags={tagsHierarchy}
-                                       multiSelection={false}
-                                       onChange={this.handleSelectTag}
-                                       selectedTags={this.state.selectedTag}
-                        />
+						<Segment>
+							<TagsSelection
+								tags={tagsHierarchy}
+								multiSelection={false}
+								onChange={this.handleSelectTag}
+								selectedTags={this.state.selectedTag}
+							/>
 						</Segment>
-                        <Segment style={{flex: "inherit"}}>
-                            <Button basic negative onClick={()=>this.setState({selectedTag: null})} content={'X'} icon={"trash"} />
-                        </Segment>
+						<Segment style={{ flex: 'inherit' }}>
+							<Button
+								basic
+								negative
+								onClick={() => this.setState({ selectedTag: null })}
+								content={'X'}
+								icon={'trash'}
+							/>
+						</Segment>
 					</Segment.Group>
 				</SegmentGroup>
 				{tags &&
-				!!Object.keys(groupedExpenses).length &&
-				(this.state.selectedTag? this.renderSingleTagCharts(groupedExpenses, tagsHierarchy):
-					<CardGroup>
-						<BarChartCard expenses={parentalGroupedExpenses} tags={tags} />
-						<PieChartCard expenses={parentalGroupedExpenses} tags={tags} />
-                	</CardGroup>)}
+					!!Object.keys(groupedExpenses).length &&
+					(this.state.selectedTag ? (
+						this.renderSingleTagCharts(groupedExpenses, tagsHierarchy)
+					) : (
+						<CardGroup>
+							<BarChartCard expenses={parentalGroupedExpenses} tags={tagsHierarchy} />
+							<PieChartCard expenses={parentalGroupedExpenses} tags={tagsHierarchy} />
+						</CardGroup>
+					))}
 			</Container>
 		);
 	}
